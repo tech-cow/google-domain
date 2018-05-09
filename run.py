14b9608b51d8b3bf31b72a39b72f8c3a0039f4b4
yuzhoujr <junior147147@yahoo.com>
2018-05-09 08:21:02 -0700

    fix windows issues

--- run.py
+++ run.py
@@ -1,65 +1,64 @@
 #!/usr/bin/env python3
-from termcolor import colored
-import json
-import datetime
-import random
-import pprint
 import re
-import tldextract
+import sys
+import json
+import pprint
+import datetime
+import platform
+
 import requests
-sys.path.insert(0, '/Users/yui/Dropbox')
+import tldextract
+
+"""config.py"""
+if platform.system() == "Windows":
+    sys.path.insert(0, r'C:\Users\yuzhou\Dropbox')
+if platform.system() == "Darwin":
+    sys.path.insert(0, '/Users/yui/Dropbox')
 import config
 
 
 def get_dom():
-    """Get Domain List and parse into an array"""
+    """
+    Parse data from `top_1000_list.txt` and grab companies' URL names
+    :rtype: <list>
+    """
     res = []
-    with open('list.txt') as f:
+    with open('top_1000_list.txt', encoding='utf-8', mode='r') as f:
         lines = f.readlines()
         for line in lines:
-            url = "".join(re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', line))
+            url = "".join(re.findall(
+                'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', line))
             domain = tldextract.extract(url).domain
             res.append("".join(domain))
         return res
 
+
 def dom_check(names):
-    """Check Availibility of the array"""
+    """
+    Looping over each company and check availibility of its Google Domain
+    :rtype: <txt file>
+    """
     for name in names:
         company = name + ".app"
-        response = requests.get("https://domainr.p.mashape.com/v2/status?mashape-key=&domain=" + company,
-          headers={
-            "X-Mashape-Key": config.mashape_key,
-            "Accept": "application/json"
-          }
-        )
+        response = requests.get(
+            "https://domainr.p.mashape.com/v2/status?mashape-key=&domain=" +
+            company,
+            headers={
+                "X-Mashape-Key": config.mashape_key,
+                "Accept": "application/json"})
         temp_dict = json.loads(response.text)
         status = temp_dict["status"][0]["status"]
 
         if status.endswith('inactive'):
-            symbol = '✓'
-            output(company, symbol)
-        else:
-            symbol = 'x'
-            output(company, symbol)
+            symbol = '✓'
+            with open('check_list.txt', encoding='utf-8', mode='a') as f:
+                f.write(company + ' ' + symbol + ' ' + '\n')
 
 
-def output(name, status):
-    with open('richlist.txt', 'a') as f:
-        f.write(name + ' ' + status + ' ' + '\n')
-
-# url = 'http://maps.googleapis.com/maps/api/directions/json'
-
-def sort():
-    with open('richlist.txt', 'r') as f:
-        lines = f.readlines()
-        for line in lines:
-            if "✓" in line:
-                with open('rich.txt', 'a') as f:
-                    f.write(line[:-5] + '\n')
+def main():
+    names = get_dom()
+    dom_check(names)
 
 
 if __name__ == '__main__':
-    # print(dom_check())
-    # name = get_dom()
-    # dom_check(name)
-    sort()
+    main()
