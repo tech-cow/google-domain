0a22de3a5c70f687488128fe1d572eab47b359fa
Yu Zhou <junior147147@yahoo.com>
2018-05-21 10:37:42 -0700

    .

--- run.py
+++ run.py
@@ -18,7 +18,10 @@ def get_dom():
     with open('list.txt') as f:
         lines = f.readlines()
         for line in lines:
-            url = "".join(re.findall('https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+', line))
+            url = "".join(
+                re.findall(
+                    'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+',
+                    line))
             domain = tldextract.extract(url).domain
             res.append("".join(domain))
         return res
@@ -28,12 +31,12 @@ def dom_check(names):
     """Check Availibility of the array"""
     for name in names:
         company = name + ".app"
-        response = requests.get("https://domainr.p.mashape.com/v2/status?mashape-key=&domain=" + company,
-          headers={
-            "X-Mashape-Key": config.mashape_key,
-            "Accept": "application/json"
-          }
-        )
+        response = requests.get(
+            "https://domainr.p.mashape.com/v2/status?mashape-key=&domain=" +
+            company,
+            headers={
+                "X-Mashape-Key": config.mashape_key,
+                "Accept": "application/json"})
         temp_dict = json.loads(response.text)
         status = temp_dict["status"][0]["status"]
 
@@ -50,6 +53,7 @@ def output(name, status):
         f.write(name + ' ' + status + ' ' + '\n')
 
 # url = 'http://maps.googleapis.com/maps/api/directions/json'
+
 
 def sort():
     with open('richlist.txt', 'r') as f:
